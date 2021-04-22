a2c48b831ee3721fbdebc39f171bf670aa27674f
Setiawan Joddy <37212064+setiawanjoddy@users.noreply.github.com>
2021-04-22 22:57:01 +0700

    Making an API Call

--- CovID/ViewController.swift
+++ CovID/ViewController.swift
@@ -11,9 +11,33 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        getData()
         // Do any additional setup after loading the view.
+    }
+    
+    func getData() {
+        let url = URL(string: "https://api.kawalcorona.com/indonesia/#")!
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            do {
+                let covid = try JSONDecoder().decode([CovidData].self, from: data!)
+                for coronavirus in covid {
+                    let positif = coronavirus.positif
+                    print(positif)
+                }
+            } catch {
+                print(error)
+            }
+        }.resume()
+        
     }
 
 
 }
 
+struct CovidData: Decodable {
+    let positif: String
+    let sembuh: String
+    let meninggal: String
+    let dirawat: String
+}
