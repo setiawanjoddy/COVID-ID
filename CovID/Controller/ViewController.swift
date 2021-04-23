8f5a679b65a916c179a3fd22c55b8f3d679e0106
Setiawan Joddy <37212064+setiawanjoddy@users.noreply.github.com>
2021-04-23 17:22:24 +0700

    Doing UI Stuff

--- CovID/Controller/ViewController.swift
+++ CovID/Controller/ViewController.swift
@@ -13,17 +13,60 @@ class ViewController: UIViewController {
     @IBOutlet weak var jumlahDirawatLabel: UILabel!
     @IBOutlet weak var jumlahSembuhLabel: UILabel!
     @IBOutlet weak var jumlahMeninggalLabel: UILabel!
+    @IBOutlet weak var positifInformationView: UIView!
+    @IBOutlet weak var dirawatInformationView: UIView!
+    @IBOutlet weak var sembuhInformationView: UIView!
+    @IBOutlet weak var meninggalInformationView: UIView!
     
     var covidManager = CovidManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        updateUI()
         covidManager.delegate = self
         covidManager.getData()
         // Do any additional setup after loading the view.
     }
     
+    func updateUI() {
+        positifInformationView.layer.cornerRadius = 15
+        positifInformationView.layer.applySketchShadow()
+        
+        dirawatInformationView.layer.cornerRadius = 15
+        dirawatInformationView.layer.applySketchShadow()
+        
+        sembuhInformationView.layer.cornerRadius = 15
+        sembuhInformationView.layer.applySketchShadow()
+        
+        meninggalInformationView.layer.cornerRadius = 15
+        meninggalInformationView.layer.applySketchShadow()
+    }
+    
+}
+
+extension CALayer {
+  func applySketchShadow(
+    color: UIColor = .black,
+    alpha: Float = 0.5,
+    x: CGFloat = 2,
+    y: CGFloat = 2,
+    blur: CGFloat = 4,
+    spread: CGFloat = 0)
+  {
+    masksToBounds = false
+    shadowColor = color.cgColor
+    shadowOpacity = alpha
+    shadowOffset = CGSize(width: x, height: y)
+    shadowRadius = blur / 2.0
+    if spread == 0 {
+      shadowPath = nil
+    } else {
+      let dx = -spread
+      let rect = bounds.insetBy(dx: dx, dy: dx)
+      shadowPath = UIBezierPath(rect: rect).cgPath
+    }
+  }
 }
 
 extension ViewController: CovidManagerDelegate {
